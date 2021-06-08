da979c7b430189756ee43101272059868ea1e0a1
shayantabrizi <shayantabrizi@gmail.com>
2021-06-08 16:50:45 +0430

    @CompositeProperty issue

--- src/main/java/com/neo4j/sdn/Person.java
+++ src/main/java/com/neo4j/sdn/Person.java
@@ -3,9 +3,15 @@ package com.neo4j.sdn;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.neo4j.driver.Value;
+import org.neo4j.driver.Values;
+import org.neo4j.driver.internal.value.NullValue;
+import org.springframework.data.neo4j.core.convert.Neo4jConversionService;
+import org.springframework.data.neo4j.core.convert.Neo4jPersistentPropertyToMapConverter;
 import org.springframework.data.neo4j.core.schema.*;
 
-import java.util.Objects;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @NoArgsConstructor
@@ -19,47 +25,44 @@ public class Person {
     private UUID id;
     @Property
     private String name;
-    @Property
-    private String family;
-    @Property
-    private int age;
-    @Relationship(type = "Member Of",direction = Relationship.Direction.OUTGOING)
-    private Member member;
+    @CompositeProperty(converter = SourceConverter.class)
+    Source source;
 
-    public Person(String name, String family, int age, Member member) {
+    public Person(String name, Source source) {
         this.name = name;
-        this.family = family;
-        this.age = age;
-        this.member = member;
+        this.source = source;
     }
 
-    public Person(Person person) {
-        this.name = person.name;
-        this.family = person.family;
-        this.age = person.age;
-        this.member=person.member;
-    }
+    public static class SourceConverter implements Neo4jPersistentPropertyToMapConverter<String, Source> {
 
-    public Person withId(UUID uuid) {
-        if (uuid.equals(id))
-            return this;
-        Person person = new Person(this);
-        person.id = uuid;
-        return person;
-    }
+        @Override
+        public Source compose(Map<String, Value> map, Neo4jConversionService neo4jConversionService) {
+            Value userId = map.get("source_userId");
+            Value userName = map.get("source_userName");
+            Value size = map.get("source_size");
 
+            if (userId != null || userName != null || size != null) {
+                return new Source(
+                        userId != null ? userId.asString() : null,
+                        userName != null ? userName.asString() : null,
+                        size != null ? size.asLong() : null
+                );
+            } else {
+                return null;
+            }
+        }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Person person = (Person) o;
-        return age == person.age && Objects.equals(id, person.id) && Objects.equals(name, person.name) && Objects.equals(family, person.family);
-    }
+        @Override
+        public Map<String, Value> decompose(Source source, Neo4jConversionService neo4jConversionService) {
+            Map map = new LinkedHashMap<>();
+            if (source != null) {
+                map.put("source_userId", source.getUserId() != null ? Values.value(source.getUserId()) : NullValue.NULL);
+                map.put("source_userName", source.getUserName() != null ? Values.value(source.getUserName()) : NullValue.NULL);
+                map.put("source_size", source.getSize() != null ? Values.value(source.getSize()) : NullValue.NULL);
+            }
+            return map;
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, family, age);
+        }
     }
 }
 
--- src/main/java/com/neo4j/sdn/StartUp.java
+++ src/main/java/com/neo4j/sdn/StartUp.java
@@ -1,52 +1,21 @@
 package com.neo4j.sdn;
 
-import org.neo4j.configuration.GraphDatabaseSettings;
-import org.neo4j.configuration.connectors.BoltConnector;
-import org.neo4j.configuration.helpers.SocketAddress;
-import org.neo4j.dbms.api.DatabaseManagementService;
-import org.neo4j.dbms.api.DatabaseManagementServiceBuilder;
-import org.neo4j.driver.*;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.data.neo4j.core.Neo4jTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.nio.file.Paths;
-import java.util.Optional;
-import java.util.Random;
 
 @Component
 public class StartUp implements ApplicationListener<ContextRefreshedEvent> {
-    @Autowired
-    Neo4jTemplate template;
-    @Autowired
-    PersonRepository repository;
-    @Autowired
-    Driver driver;
+    private final AService aService;
+
+    public StartUp(AService aService) {
+        this.aService = aService;
+    }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        method();
+        aService.method();
 
     }
 
-    @Transactional
-    void method() {
-            template.deleteAll(Person.class);
-            template.deleteAll(Club.class);
-            // save by Neo4jTemplate
-            Person person_1 = template.save(new Person("Reza", "Mahdavi", 22, new Member(new Club("Iran"), "2021")));
-            // Save by Repository
-            Person person_2 = repository.save(new Person("Ali" + new Random(System.currentTimeMillis()).nextInt(), "Alavi", 20, new Member(new Club("Iran"), "2020")));
-            // Query by Repository
-            Person ali = repository.findByName("Ali");
-            // Query by Neo4jTemplate
-            Optional<Person> reza = template.findById(person_1.getId(), Person.class);
-            // Custom query
-            Result result = driver.session().run("Match(n) return count(n) as count", TransactionConfig.builder().build());
-            System.out.println("Entity Count: " + result.single().values().get(0));
-    }
 }
